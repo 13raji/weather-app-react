bc75ce6e8b64cb902d4aa042ff76a6561b62c183
13raji <70148189+13raji@users.noreply.github.com>
2020-11-24 14:58:05 +0000

    added CityLinks js + css

---
+++ src/CityLinks.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./CityLinks.css";
+
+export default function CityLinks() {
+  return (
+    <div className="row cityDivert">
+      <div className="col-2">
+        <a href="/">Cardiff</a>
+      </div>
+      <div className="col-2">
+        <a href="/">Melbourne</a>
+      </div>
+      <div className="col-2">
+        <a href="/">New York</a>
+      </div>
+      <div className="col-2">
+        <a href="/">Florence</a>
+      </div>
+      <div className="col-2">
+        <a href="/">Barrow</a>
+      </div>
+      <div className="col-2">
+        <a href="/">Dehli</a>
+      </div>
+    </div>
+  );
+}
